9eae2b1c0466479f5477cb4975314ca399ac79b5
Rakesh3555 <166705071+Rakesh3555@users.noreply.github.com>
2024-04-11 15:49:33 +0530

    Testfare.java

--- Taxi/src/com/cab/day6/Testfare.java
+++ Taxi/src/com/cab/day6/Testfare.java
@@ -5,17 +5,11 @@ import java.util.Scanner;
 public class Testfare {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-			
-			Scanner dc = new Scanner(System.in);
+		Scanner dc = new Scanner(System.in);
 			cab tf = new cab();	
 			double amount=tf.cabFare();
 			System.out.println("the amount "+amount);
 	        discountCalculator.discab(amount);
-			
-				
-
 	}
 
 }
