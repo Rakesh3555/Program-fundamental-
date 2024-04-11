83b36b8d6cf27667708e5b2cb7d9464c203438ab
Rakesh3555 <166705071+Rakesh3555@users.noreply.github.com>
2024-04-11 15:25:55 +0530

    Add files via upload

---
+++ day1/College.java
@@ -0,0 +1,12 @@
+package com.chainsys.day1;
+
+public class College {
+
+	public static void main(String[] args) {
+		System.out.println("College Names : Kamaraj college of engg & tech");
+		System.out.println("College Code : 9256");
+		System.out.println("College Location : Viruthunagar");
+		
+	}
+
+}
---
+++ day1/Details.java
@@ -0,0 +1,14 @@
+package com.chainsys.day1;
+
+public class Details {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Rakesh\n22");
+		System.out.println("Madurai");
+		System.out.println("8610*******" + "\n" + "Chainsys");
+
+	}
+
+}
