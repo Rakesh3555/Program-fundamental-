8f35fc81a0ce3bca9cb78140bbdb71a47a11e3ec
Rakesh3555 <rakesh.arunagirisamy@chainsys.com>
2024-04-13 12:31:57 +0530

    Hackerrank code

---
+++ HackerRank/HelloHr.java
@@ -0,0 +1,9 @@
+package com.chainsys.HackerRank;
+
+public class HelloHr {
+    public static void main (String [] agrs){
+                /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+                System.out.println("Hello, World.");
+                System.out.println("Hello, Java.");
+            }
+        }
